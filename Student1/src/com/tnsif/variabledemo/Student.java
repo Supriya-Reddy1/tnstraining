1b65058bfcc14906aab3e1d6c1feaad94a7e2580
supriyareddy <supriyareddy@192.168.200.75>
2025-04-30 12:52:23 +0530

    First commit

---
+++ Student1/src/com/tnsif/variabledemo/Student.java
@@ -0,0 +1,13 @@
+package com.tnsif.variabledemo;
+
+public class Student {
+	int sid;
+	String sname;
+	static String collegename="Sri indu";
+	 void display() {
+		 System.out.println("The details of the student"+sid+","+sname+","+collegename);
+	 }
+}
+
+
+
