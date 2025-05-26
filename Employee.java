f1c8295fda620f57f07937081681201200373508
Supriya-Reddy1 <supriya76600@gmail.com>
2025-05-26 17:58:18 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,21 @@
+package com.tnsif.comparator;
+
+public class Employee {
+		public int eid;
+		public String ename;
+		public float sal;
+		
+		public Employee(int eid, String ename, float sal) {
+			super();
+			this.eid = eid;
+			this.ename = ename;
+			this.sal = sal;
+		}
+		@Override
+		public String toString() {
+			return "Employee [eid=" + eid + ", ename=" + ename + ", sal=" + sal + "]";
+		}
+	}
+
+
+
---
+++ EmployeeNameCom.java
@@ -0,0 +1,14 @@
+package com.tnsif.comparator;
+
+import java.util.Comparator;
+
+public class EmployeeNameCom implements Comparator<Employee>{
+
+		@Override
+		public int compare(Employee e1, Employee e2) {
+			return e1.ename.compareTo(e2.ename);
+		}
+	}
+
+
+
